c643bad276f2d9e5e39c88117b096eaefaadae50
Marino09 <me.martinezp9@gmail.com>
2014-11-10 23:57:57 -0500

    Now where, same results

--- hello.hs
+++ hello.hs
@@ -1,4 +1,11 @@
-alwaysEven a b = let isEven x = if even x
-                                 then x
-                                 else x - 1
-                              in (isEven a, isEven b)
+--using let
+
+--alwaysEven a b = let isEven x = if even x
+--                                 then x
+--                                 else x - 1
+--                              in (isEven a, isEven b)
+
+--using where
+
+alwaysEven a b = (isEven a, isEven b)
+  where isEven x = if even x then x else x-1 
